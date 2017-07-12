d2cf295f1d21c2f20963322369eaeabb2c75381f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-07-12 08:58:02 +0100

    Remove more unused code

---
+++ src/SFA.DAS.EmployerCommitments.Domain/Models/Organisation/LegalEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerCommitments.Domain.Models.Organisation
+{
+    public class LegalEntity
+    {
+        public string Name { get; set; }
+        public string RegisteredAddress { get; set; }
+        public short Source { get; set; }
+    }
+}
