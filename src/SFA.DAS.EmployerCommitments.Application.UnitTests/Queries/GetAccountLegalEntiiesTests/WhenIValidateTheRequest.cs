a968c2e552b65c0749968beea6b35a0c0cb46834
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-07-13 09:53:14 +0100

    Change validator to check user on account

--- src/SFA.DAS.EmployerCommitments.Application.UnitTests/Queries/GetAccountLegalEntiiesTests/WhenIValidateTheRequest.cs
+++ src/SFA.DAS.EmployerCommitments.Application.UnitTests/Queries/GetAccountLegalEntiiesTests/WhenIValidateTheRequest.cs
@@ -1,39 +1,65 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
 using SFA.DAS.EmployerCommitments.Application.Queries.GetAccountLegalEntities;
+using SFA.DAS.EmployerCommitments.Domain.Interfaces;
+using SFA.DAS.EmployerCommitments.Domain.Models.AccountTeam;
 
 namespace SFA.DAS.EmployerCommitments.Application.UnitTests.Queries.GetAccountLegalEntiiesTests
 {
     public class WhenIValidateTheRequest
     {
         private GetAccountLegalEntitiesValidator _validator;
+        private Mock<IEmployerAccountService> _employerAccountApi;
 
         [SetUp]
         public void Arrange()
         {
-            _validator = new GetAccountLegalEntitiesValidator();
+            _employerAccountApi = new Mock<IEmployerAccountService>();
+
+            _employerAccountApi.Setup(x => x.GetUserRoleOnAccount(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(new TeamMember {UserRef = "ref"});
+
+            _validator = new GetAccountLegalEntitiesValidator(_employerAccountApi.Object);
         }
 
         [Test]
-        public void ThenTrueIsReturnedWhenAllFieldsArePopulated()
+        public async Task ThenTrueIsReturnedWhenAllFieldsArePopulated()
         {
             //Act
-            var actual = _validator.Validate(new GetAccountLegalEntitiesRequest { HashedAccountId = "123ABD", UserId = "123HGB", SignedOnly = false});
+            var actual = await _validator.ValidateAsync(new GetAccountLegalEntitiesRequest { HashedAccountId = "123ABD", UserId = "123HGB", SignedOnly = false});
 
             //Assert
             Assert.IsTrue(actual.IsValid());
         }
 
         [Test]
-        public void ThenFalseIsReturnedAndTheErrorDictionaryPopulatedWhenTheRequestIsNotPopulated()
+        public async Task ThenFalseIsReturnedAndTheErrorDictionaryPopulatedWhenTheRequestIsNotPopulated()
         {
             //Act
-            var actual = _validator.Validate(new GetAccountLegalEntitiesRequest());
+            var actual = await _validator.ValidateAsync(new GetAccountLegalEntitiesRequest());
 
             //Assert
             Assert.IsFalse(actual.IsValid());
             Assert.Contains(new KeyValuePair<string,string>("HashedAccountId", "HashedAccountId has not been supplied"), actual.ValidationDictionary);
             Assert.Contains(new KeyValuePair<string,string>("UserId", "UserId has not been supplied"), actual.ValidationDictionary);
+            _employerAccountApi.Verify(x => x.GetUserRoleOnAccount(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+        }
+
+        [Test]
+        public async Task ThenIfTheUSerIsNotConnectedtoTheAccountTheValidationResultIsMarkedAsNotAuthorized()
+        {
+            //Arrange
+            var expectedAccountId = "123ABD";
+            var expectedUserId = "123HGB";
+            _employerAccountApi.Setup(x => x.GetUserRoleOnAccount(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(null);
+
+            //Act
+            var actual = await _validator.ValidateAsync(new GetAccountLegalEntitiesRequest { HashedAccountId = expectedAccountId, UserId = expectedUserId, SignedOnly = false });
+
+            //Assert
+            _employerAccountApi.Verify(x => x.GetUserRoleOnAccount(expectedAccountId, expectedUserId), Times.Once);
+            Assert.IsTrue(actual.IsUnauthorized);
         }
 
     }
--- src/SFA.DAS.EmployerCommitments.Application/Queries/GetAccountLegalEntities/GetAccountLegalEntitiesValidator.cs
+++ src/SFA.DAS.EmployerCommitments.Application/Queries/GetAccountLegalEntities/GetAccountLegalEntitiesValidator.cs
@@ -1,12 +1,26 @@
 ﻿using System.Threading.Tasks;
 using SFA.DAS.EmployerCommitments.Application.Validation;
+using SFA.DAS.EmployerCommitments.Domain.Interfaces;
 
 namespace SFA.DAS.EmployerCommitments.Application.Queries.GetAccountLegalEntities
 {
     public class GetAccountLegalEntitiesValidator : IValidator<GetAccountLegalEntitiesRequest>
     {
+        private readonly IEmployerAccountService _employerAccountService;
+
+        public GetAccountLegalEntitiesValidator(IEmployerAccountService employerAccountService)
+        {
+            _employerAccountService = employerAccountService;
+        }
+
         public ValidationResult Validate(GetAccountLegalEntitiesRequest item)
         {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<ValidationResult> ValidateAsync(GetAccountLegalEntitiesRequest item)
+        {
+            
             var validationResult = new ValidationResult();
 
             if (string.IsNullOrEmpty(item.UserId))
@@ -19,12 +33,19 @@ namespace SFA.DAS.EmployerCommitments.Application.Queries.GetAccountLegalEntitie
                 validationResult.AddError(nameof(item.HashedAccountId));
             }
 
-            return validationResult;
-        }
+            if (!validationResult.IsValid())
+            {
+                return validationResult;
+            }
 
-        public Task<ValidationResult> ValidateAsync(GetAccountLegalEntitiesRequest item)
-        {
-            throw new System.NotImplementedException();
+            var result = await _employerAccountService.GetUserRoleOnAccount(item.HashedAccountId, item.UserId);
+
+            if (result == null)
+            {
+                validationResult.IsUnauthorized = true;
+            }
+
+            return validationResult;
         }
     }
 }
