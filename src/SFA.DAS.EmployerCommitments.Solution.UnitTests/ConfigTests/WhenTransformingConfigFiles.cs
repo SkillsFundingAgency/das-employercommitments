3a7dd411cd9b2a6bd3ab1c92ddb6378ed4c0eba4
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-07-14 21:20:59 +0100

    Add cloud config allowed setting
    Allow the setting of the EmployerAccountBaseUrl in the cloud
    configuration file

--- src/SFA.DAS.EmployerCommitments.Solution.UnitTests/ConfigTests/WhenTransformingConfigFiles.cs
+++ src/SFA.DAS.EmployerCommitments.Solution.UnitTests/ConfigTests/WhenTransformingConfigFiles.cs
@@ -19,7 +19,7 @@ namespace SFA.DAS.EmployerCommitments.Solution.UnitTests.ConfigTests
             var extensions = new[] {".config", ".cscfg"};
             var excludedPaths = new[] { "obj", "bin", "vs", "package", "tool", "test" };
 
-            _excludedSettingNames = new[] { "webpages:Version", "LogLevel", "idaAudience", "idaTenant", "TokenCertificateThumbprint", "DeclarationsEnabled", "CurrentTime" };
+            _excludedSettingNames = new[] { "webpages:Version", "LogLevel", "idaAudience", "idaTenant", "TokenCertificateThumbprint", "DeclarationsEnabled", "CurrentTime", "EmployerAccountBaseUrl" };
             _allowedConfigValues = new[] { "UseDevelopmentStorage=true", "LOCAL", "true", "false", "localhost", "Endpoint=sb://[your namespace].servicebus.windows.net;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=[your secret]" };
 
             var path = new FileInfo(Assembly.GetCallingAssembly().Location).Directory.Parent.Parent.Parent;
