52d12f40a9c7dd97c9ff14add5b2a9ef74b71b72
Lee Wadhams <lee.wadhams@valtech.co.uk>
2017-09-12 13:35:23 +0100

    Removed unused properties

--- src/SFA.DAS.EAS.Web/Plumbing/Mvc/GoogleTagManagerFilterAttribute.cs
+++ src/SFA.DAS.EAS.Web/Plumbing/Mvc/GoogleTagManagerFilterAttribute.cs
@@ -42,16 +42,9 @@ namespace SFA.DAS.EmployerCommitments.Web.Plumbing.Mvc
 
         public class GaTagData
         {
-            public string DataLoaded { get; set; } = "dataLoaded";
             public bool IsAuthenticated { get; set; }
             public string UserId { get; set; }
-            public string HashedAccountId { get; set; }
-
-            public string Vpv { get; set; }
             public string Acc { get; set; }
-            public string Org { get; set; }
-
-            public IDictionary<string, string> Extras { get; set; } = new Dictionary<string, string>();
         }
     }
 }
