c4dba3c99880006cacce7ac4db178ef84dd5e609
Lee Wadhams <lee.wadhams@valtech.co.uk>
2017-10-04 12:25:07 +0100

    Renamed dist folder and updated paths to use this

--- src/SFA.DAS.EAS.Web/App_Start/BundleConfig.cs
+++ src/SFA.DAS.EAS.Web/App_Start/BundleConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Optimization;
+﻿using Microsoft.Azure;
+using System;
+using System.Web.Optimization;
 
 namespace SFA.DAS.EmployerCommitments.Web
 {
@@ -7,43 +9,44 @@ namespace SFA.DAS.EmployerCommitments.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/sfajs").Include(
-                      "~/dist/javascripts/jquery-1.11.0.min.js",
-                      "~/dist/javascripts/govuk-template.js",
-                      "~/dist/javascripts/selection-buttons.js",
-                      "~/dist/javascripts/showhide-content.js",
-                      "~/dist/javascripts/stacker.js",
-                      "~/dist/javascripts/app.js"));  
+            var commitmentsAssetsFolder = CloudConfigurationManager.GetSetting("CommitmentsAssetsFolder");
 
-            bundles.Add(new ScriptBundle("~/bundles/apprentice").Include(
-                      "~/dist/javascripts/apprentice/select2.min.js",
-                      "~/dist/javascripts/apprentice/dropdown.js"
-                   
+            bundles.Add(new ScriptBundle("~/comt-dist/bundles/sfajs").Include(
+                      "~/comt-dist/javascripts/jquery-1.11.0.min.js",
+                      "~/comt-dist/javascripts/govuk-template.js",
+                      "~/comt-dist/javascripts/selection-buttons.js",
+                      "~/comt-dist/javascripts/showhide-content.js",
+                      "~/comt-dist/javascripts/stacker.js",
+                      "~/comt-dist/javascripts/app.js"));
+
+            bundles.Add(new ScriptBundle("~/comt-dist/bundles/apprentice").Include(
+                      "~/comt-dist/javascripts/apprentice/select2.min.js",
+                      "~/comt-dist/javascripts/apprentice/dropdown.js"
+
                       ));
 
-            bundles.Add(new ScriptBundle("~/bundles/characterLimitation").Include(                
-                    "~/dist/javascripts/character-limit.js"
+            bundles.Add(new ScriptBundle("~/comt-dist/bundles/characterLimitation").Include(
+                    "~/comt-dist/javascripts/character-limit.js"
                     ));
 
-            bundles.Add(new ScriptBundle("~/bundles/lengthLimitation").Include(
-                    "~/dist/javascripts/length-limit.js"
+            bundles.Add(new ScriptBundle("~/comt-dist/bundles/lengthLimitation").Include(
+                    "~/comt-dist/javascripts/length-limit.js"
                     ));
 
-            bundles.Add(new ScriptBundle("~/bundles/paymentOrder").Include(
-                  "~/dist/javascripts/payment-order.js"
+            bundles.Add(new ScriptBundle("~/comt-dist/bundles/paymentOrder").Include(
+                  "~/comt-dist/javascripts/payment-order.js"
                  ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryvalcustom").Include(
+            bundles.Add(new ScriptBundle("~/comt-dist/bundles/jqueryvalcustom").Include(
                       "~/Scripts/jquery.validate.js", "~/Scripts/jquery.validate.unobtrusive.custom.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/lodash").Include(
-                        "~/dist/javascripts/lodash.js"));
+            bundles.Add(new ScriptBundle("~/comt-dist/bundles/lodash").Include(
+                        "~/comt-dist/javascripts/lodash.js"));
 
-            bundles.Add(new StyleBundle("~/bundles/screenie6").Include("~/dist/css/screen-ie6.css"));
-            bundles.Add(new StyleBundle("~/bundles/screenie7").Include("~/dist/css/screen-ie7.css"));
-            bundles.Add(new StyleBundle("~/bundles/screenie8").Include("~/dist/css/screen-ie8.css"));
-            bundles.Add(new StyleBundle("~/bundles/screen").Include("~/dist/css/screen.css"));
-
+            bundles.Add(new StyleBundle("~/comt-dist/bundles/screenie6").Include("~/comt-dist/css/screen-ie6.css"));
+            bundles.Add(new StyleBundle("~/comt-dist/bundles/screenie7").Include("~/comt-dist/css/screen-ie7.css"));
+            bundles.Add(new StyleBundle("~/comt-dist/bundles/screenie8").Include("~/comt-dist/css/screen-ie8.css"));
+            bundles.Add(new StyleBundle("~/comt-dist/bundles/screen").Include("~/comt-dist/css/screen.css"));
         }
     }
 }
