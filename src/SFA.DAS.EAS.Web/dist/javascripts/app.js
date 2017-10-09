f0cc17cae746bbe4b858d539ce966acb4642ff72
RajeshV <rajesh@digi2al.co.uk>
2017-10-09 14:35:39 +0100

    GA pushes for Error message and confirmation messages.

--- src/SFA.DAS.EAS.Web/dist/javascripts/app.js
+++ src/SFA.DAS.EAS.Web/dist/javascripts/app.js
@@ -252,3 +252,28 @@ $("#search-input").on("blur", (function () {
         $(this).val(placeholderText);
     }
 }));
+
+// Push confirmation messages to the Google dateLayer array
+var successMessage = $('.success-summary h1');
+if (successMessage.length > 0) {
+    var dataLoadedObj = dataLayer[0];
+    if (dataLoadedObj.event === 'dataLoaded') {
+        dataLoadedObj.success = successMessage.text();
+        dataLayer[0] = dataLoadedObj;
+    }
+}
+
+
+// Push error messages to the Google dateLayer array
+
+var errorMessage = $('.error-summary');
+
+if (errorMessage.length > 0) {
+    var errorContent = errorMessage.find('ul li a').eq(0).text(),
+        dataLoadedObj = dataLayer[0];
+
+    if (dataLoadedObj.event === 'dataLoaded') {
+        dataLoadedObj.error_message = errorContent;
+        dataLayer[0] = dataLoadedObj;
+    }
+}
