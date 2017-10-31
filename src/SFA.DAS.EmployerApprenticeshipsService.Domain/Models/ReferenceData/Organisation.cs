a95f0831335ab6b18efe4a8bd7abbbc5afd6825d
Iain.Stevenson <Iain.Stevenson@live.co.uk>
2017-10-31 09:21:05 +0000

    Finished Enum change over to common version

--- src/SFA.DAS.EmployerApprenticeshipsService.Domain/Models/ReferenceData/Organisation.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Models/ReferenceData/Organisation.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using SFA.DAS.Common.Domain.Types;
 using SFA.DAS.EmployerCommitments.Domain.Models.Organisation;
 
 namespace SFA.DAS.EmployerCommitments.Domain.Models.ReferenceData
@@ -7,8 +6,8 @@ namespace SFA.DAS.EmployerCommitments.Domain.Models.ReferenceData
     public class Organisation
     {
         public string Name { get; set; }
-        public OrganisationType Type { get; set; }
-        public OrganisationSubType SubType { get; set; }
+        public SFA.DAS.Common.Domain.Types.OrganisationType Type { get; set; }
+        public SFA.DAS.Common.Domain.Types.OrganisationSubType SubType { get; set; }
         public string Code { get; set; }
         public DateTime? RegistrationDate { get; set; }
         public Address Address { get; set; }
