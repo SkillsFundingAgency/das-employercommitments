93181538c9f1f3ce21f10c51736f1397b0d2be45
phildavies <github@phildavies.co.uk>
2018-03-29 14:12:36 +0100

    add unit test for GetYourCohort method in orchestrator

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Orchestrators/EmployerCommitmentOrchestrator/WhenGettingYourCohort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Commitments.Api.Types.Commitment;
+using SFA.DAS.EmployerCommitments.Application.Queries.GetCommitments;
+
+namespace SFA.DAS.EmployerCommitments.Web.UnitTests.Orchestrators.EmployerCommitmentOrchestrator
+{
+    [TestFixture]
+    public class WhenGettingYourCohort : OrchestratorTestBase
+    {
+        [Test]
+        public async Task ThenAllCountsShouldBeZeroIfNoCommitments()
+        {
+            //Arrange
+            MockMediator.Setup(x => x.SendAsync(It.IsAny<GetCommitmentsQuery>()))
+                .ReturnsAsync(new GetCommitmentsResponse
+                {
+                    Commitments = new List<CommitmentListItem>()
+                });
+
+            //Act
+            var result = await EmployerCommitmentOrchestrator.GetYourCohorts("ABC123", "ABC321");
+
+            //Assert
+            Assert.AreEqual(0, result.Data.DraftCount);
+            Assert.AreEqual(0, result.Data.ReadyForReviewCount);
+            Assert.AreEqual(0, result.Data.WithProviderCount);
+            Assert.AreEqual(0, result.Data.TransferFundedCohortsCount);
+        }
+    }
+}
