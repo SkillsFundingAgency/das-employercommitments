cb74449859f1733cc465b93424a4e1717064d439
chrisfoster76 <chrisfoster186@googlemail.com>
2018-04-06 11:43:03 +0100

    Correct edit link Unit tests for new scenarios (failing)

--- src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Orchestrators/EmployerCommitmentOrchestrator/WhenGettingCommitmentDetails.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Orchestrators/EmployerCommitmentOrchestrator/WhenGettingCommitmentDetails.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
+using SFA.DAS.Commitments.Api.Types;
 using SFA.DAS.Commitments.Api.Types.Commitment;
+using SFA.DAS.Commitments.Api.Types.Commitment.Types;
+using SFA.DAS.EmployerCommitments.Application.Exceptions;
 using SFA.DAS.EmployerCommitments.Application.Queries.GetFrameworks;
 using SFA.DAS.EmployerCommitments.Application.Queries.GetStandards;
 using SFA.DAS.EmployerCommitments.Application.Queries.GetTrainingProgrammes;
@@ -35,5 +39,48 @@ namespace SFA.DAS.EmployerCommitments.Web.UnitTests.Orchestrators.EmployerCommit
             MockMediator.Verify(x => x.SendAsync(It.IsAny<GetTrainingProgrammesQueryRequest>()), Times.Once);
         }
 
+        [Test]
+        public async Task ThenIfTheCohortHasBeenRejectedByTransferSenderThenItIsEditable()
+        {
+            CommitmentView.AgreementStatus = AgreementStatus.BothAgreed;
+            CommitmentView.EditStatus = EditStatus.EmployerOnly;
+            CommitmentView.TransferSender = new TransferSender
+            {
+                TransferApprovalStatus = TransferApprovalStatus.Rejected
+            };
+
+            var result = await EmployerCommitmentOrchestrator.GetCommitmentDetails("HashedAccId", "HashedCmtId", "ExtUserId");
+
+            Assert.IsFalse(result.Data.IsReadOnly);
+        }
+
+        [Test]
+        public async Task ThenIfTheCohortIsPendingTransferApprovalThenItIsReadOnly()
+        {
+            CommitmentView.AgreementStatus = AgreementStatus.BothAgreed;
+            CommitmentView.EditStatus = EditStatus.Both;
+            CommitmentView.TransferSender = new TransferSender
+            {
+                TransferApprovalStatus = TransferApprovalStatus.Pending,
+            };
+
+            var result = await EmployerCommitmentOrchestrator.GetCommitmentDetails("HashedAccId", "HashedCmtId", "ExtUserId");
+
+            Assert.IsTrue(result.Data.IsReadOnly);
+        }
+
+        [Test]
+        public async Task ThenIfTheCohortWasApprovedByTransferSenderThenShouldThrownAnException()
+        {
+            CommitmentView.AgreementStatus = AgreementStatus.BothAgreed;
+            CommitmentView.EditStatus = EditStatus.Both;
+            CommitmentView.TransferSender = new TransferSender
+            {
+                TransferApprovalStatus = TransferApprovalStatus.Approved
+            };
+
+            Assert.ThrowsAsync<InvalidStateException>(() =>
+                EmployerCommitmentOrchestrator.GetCommitmentDetails("HashedAccId", "HashedCmtId", "ExtUserId"));
+        }
     }
 }
