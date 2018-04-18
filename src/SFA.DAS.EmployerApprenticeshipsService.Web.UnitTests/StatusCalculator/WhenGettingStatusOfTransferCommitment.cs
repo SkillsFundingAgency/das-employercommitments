eb5497c9ad96fad09ddef6b259ff94dec8bd0216
chrisfoster76 <chrisfoster186@googlemail.com>
2018-04-18 11:47:02 +0100

    Modifications to take account of Last Action "AmendAfterRejected" -> Drafts

--- src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/StatusCalculator/WhenGettingStatusOfTransferCommitment.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/StatusCalculator/WhenGettingStatusOfTransferCommitment.cs
@@ -12,18 +12,19 @@ namespace SFA.DAS.EmployerCommitments.Web.UnitTests.StatusCalculator
     [TestFixture]
     public sealed class WhenGettingStatusOfTransferCommitment
     {
-        [TestCase(RequestStatus.NewRequest, EditStatus.EmployerOnly, TransferApprovalStatus.Pending, TestName = "With receiving employer")]
-        [TestCase(RequestStatus.SentToProvider, EditStatus.ProviderOnly, TransferApprovalStatus.Pending, TestName = "With provider")]
-        [TestCase(RequestStatus.WithSenderForApproval, EditStatus.Both, TransferApprovalStatus.Pending, TestName = "With sender but not yet actioned by them")]
-        [TestCase(RequestStatus.RejectedBySender, EditStatus.EmployerOnly, TransferApprovalStatus.Rejected, TestName = "With sender, rejected by them, but not yet saved or edited")]
-        [TestCase(RequestStatus.None, EditStatus.Both, TransferApprovalStatus.Approved, TestName = "Approved by all 3 parties")]
-        public void CommitmentIsTransferFundedAndInValidState(RequestStatus expectedResult, EditStatus editStatus, TransferApprovalStatus transferApprovalStatus)
+        [TestCase(RequestStatus.NewRequest, EditStatus.EmployerOnly, TransferApprovalStatus.Pending, LastAction.None, TestName = "With receiving employer")]
+        [TestCase(RequestStatus.SentToProvider, EditStatus.ProviderOnly, TransferApprovalStatus.Pending, LastAction.None, TestName = "With provider")]
+        [TestCase(RequestStatus.WithSenderForApproval, EditStatus.Both, TransferApprovalStatus.Pending , LastAction.None, TestName = "With sender but not yet actioned by them")]
+        [TestCase(RequestStatus.RejectedBySender, EditStatus.EmployerOnly, TransferApprovalStatus.Rejected, LastAction.None, TestName = "With sender, rejected by them, but not yet saved or edited")]
+        [TestCase(RequestStatus.None, EditStatus.Both, TransferApprovalStatus.Approved, LastAction.None, TestName = "Approved by all 3 parties")]
+        [TestCase(RequestStatus.NewRequest, EditStatus.EmployerOnly, TransferApprovalStatus.Pending, LastAction.AmendAfterRejected, TestName = "Returned to draft following edit of rejected cohort")]
+        public void CommitmentIsTransferFundedAndInValidState(RequestStatus expectedResult, EditStatus editStatus, TransferApprovalStatus transferApprovalStatus, LastAction lastAction)
         {
             var commitment = new CommitmentListItem
             {
                 AgreementStatus = AgreementStatus.NotAgreed,
                 ApprenticeshipCount = 1,
-                LastAction = LastAction.None,
+                LastAction = lastAction,
                 EditStatus = editStatus,
                 TransferSenderId = 1,
                 TransferApprovalStatus = transferApprovalStatus
