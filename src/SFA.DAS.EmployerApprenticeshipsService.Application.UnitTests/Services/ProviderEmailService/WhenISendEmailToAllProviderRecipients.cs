b98fcd02abce8e1dbe09350bbc149b42621dfcdb
chrisfoster76 <chrisfoster186@googlemail.com>
2018-05-01 12:57:22 +0100

    Initial work on new service

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Services/ProviderEmailService/WhenISendEmailToAllProviderRecipients.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerCommitments.Domain.Interfaces;
+using SFA.DAS.Notifications.Api.Client;
+using SFA.DAS.Notifications.Api.Types;
+
+namespace SFA.DAS.EmployerCommitments.Application.UnitTests.Services.ProviderEmailService
+{
+    [TestFixture]
+    public class WhenISendEmailToAllProviderRecipients
+    {
+        private Application.Services.ProviderEmailService _providerEmailService;
+        private Mock<IProviderEmailLookupService> _providerEmailLookupService;
+        private Mock<INotificationsApi> _notificationsApi;
+
+        private long _providerId;
+        private string _providerLastUpdateEmailAddress;
+        private List<string> _exampleRecipients;
+        private Email _exampleValidEmail;
+
+        private Func<Task> _act;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _providerId = 1;
+            _providerLastUpdateEmailAddress = "AlternativeEmailAddress";
+
+            _exampleRecipients = new List<string> { "Recipient1", "Recipient2", "Recipient3" };
+            _providerEmailLookupService = new Mock<IProviderEmailLookupService>();
+            _providerEmailLookupService.Setup(x => x.GetEmailsAsync(It.IsAny<long>(), It.IsAny<string>()))
+                .Returns(() => new Task<List<string>>(() => _exampleRecipients));
+
+            _notificationsApi = new Mock<INotificationsApi>();
+            _notificationsApi.Setup(x => x.SendEmail(It.IsAny<Email>()))
+                .Returns(() => Task.CompletedTask);
+
+            _providerEmailService = new Application.Services.ProviderEmailService(_providerEmailLookupService.Object, _notificationsApi.Object);
+
+            _exampleValidEmail = new Email
+            {
+                TemplateId = "TemplateId",
+                Subject ="SubjectId",
+                ReplyToAddress = "ReplyToAddress",
+                Tokens = new Dictionary<string, string>
+                {
+                    { "Key1", "Value1" },
+                    { "Key2", "Value2" }
+                }
+            };
+
+            _act = () => _providerEmailService.SendEmailToAllProviderRecipients(
+                _providerId, _providerLastUpdateEmailAddress, _exampleValidEmail);
+        }
+
+        [Test]
+        public async Task ThenTheProviderEmailLookupServiceIsCalledToRetrieveRecipientAddresses()
+        {
+            await _act.Invoke();
+
+            _providerEmailLookupService.Verify(x => x.GetEmailsAsync(
+                    It.Is<long>(l =>  l == _providerId),
+                    It.Is<string>(s => s == _providerLastUpdateEmailAddress)
+                ), Times.Once);
+        }
+
+
+
+        [Test]
+        public async Task ThenTheNotificationsApiIsCalledToSendAnEmailForEachRecipient()
+        {
+            await _act.Invoke();
+
+            _notificationsApi.Verify(x => x.SendEmail(It.IsAny<Email>())
+                , Times.Exactly(_exampleRecipients.Count));
+
+        }
+
+
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Services/ProviderEmailService.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EmployerCommitments.Domain.Interfaces;
+using SFA.DAS.Notifications.Api.Client;
+using SFA.DAS.Notifications.Api.Types;
+
+namespace SFA.DAS.EmployerCommitments.Application.Services
+{
+    public class ProviderEmailService : IProviderEmailService
+    {
+        private IProviderEmailLookupService _providerEmailLookupService;
+        private readonly INotificationsApi _notificationsApi;
+
+        public ProviderEmailService(IProviderEmailLookupService providerEmailLookupService, INotificationsApi notificationsApi)
+        {
+            _providerEmailLookupService = providerEmailLookupService;
+            _notificationsApi = notificationsApi;
+        }
+        public Task SendEmailToAllProviderRecipients(long providerId, string lastUpdateEmailAddress, Email email)
+        {
+
+        //    _logger.Info($"Sending notification for commitment {commitment.Id} to providers with ukprn {commitment.ProviderId}");
+        //    var emails = await
+        //        _providerEmailLookupService.GetEmailsAsync(
+        //            commitment.ProviderId.GetValueOrDefault(),
+        //            commitment.ProviderLastUpdateInfo?.EmailAddress ?? string.Empty);
+
+        //    _logger.Info($"{emails.Count} provider found email address/es");
+        //    if (!_configuration.CommitmentNotification.SendEmail) return;
+
+        //    foreach (var email in emails)
+        //    {
+        //        _logger.Info($"Sending email to {email}");
+        //        var notificationCommand = BuildNotificationCommand(email, commitment);
+        //        await _mediator.SendAsync(notificationCommand);
+        //    }
+
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
