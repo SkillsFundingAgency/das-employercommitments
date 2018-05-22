673be693a331758867c6200d2813fc3599a761ea
phildavies <github@phildavies.co.uk>
2018-05-22 10:25:10 +0100

    wip

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/SubmitCommitment/SubmitCommitmentCommandHandler.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/SubmitCommitment/SubmitCommitmentCommandHandler.cs
@@ -7,27 +7,20 @@ using SFA.DAS.Commitments.Api.Types.Commitment;
 using SFA.DAS.Commitments.Api.Types.Commitment.Types;
 using SFA.DAS.EmployerCommitments.Application.Commands.SendNotification;
 using SFA.DAS.EmployerCommitments.Application.Exceptions;
-//using SFA.DAS.EmployerCommitments.Application.Queries.GetTransferRequest;
 using SFA.DAS.EmployerCommitments.Domain.Configuration;
 using SFA.DAS.EmployerCommitments.Domain.Interfaces;
 using SFA.DAS.NLog.Logger;
 using SFA.DAS.Notifications.Api.Types;
-//using Task = System.Threading.Tasks.Task;
 
 namespace SFA.DAS.EmployerCommitments.Application.Commands.SubmitCommitment
 {
     public sealed class SubmitCommitmentCommandHandler : AsyncRequestHandler<SubmitCommitmentCommand>
     {
         private readonly IEmployerCommitmentApi _commitmentApi;
-
         private readonly IMediator _mediator;
-
         private readonly EmployerCommitmentsServiceConfiguration _configuration;
-
         private readonly IProviderEmailLookupService _providerEmailLookupService;
-
         private readonly ILog _logger;
-
         private readonly SubmitCommitmentCommandValidator _validator;
 
         public SubmitCommitmentCommandHandler(
@@ -56,7 +49,7 @@ namespace SFA.DAS.EmployerCommitments.Application.Commands.SubmitCommitment
             var commitment = await _commitmentApi.GetEmployerCommitment(message.EmployerAccountId, message.CommitmentId);
 
             if (commitment.EmployerAccountId != message.EmployerAccountId)
-                throw new InvalidRequestException(new Dictionary<string, string> { { "Commitment", "This commiment does not belong to this Employer Account " } });
+                throw new InvalidRequestException(new Dictionary<string, string> { { "Commitment", "This commitment does not belong to this Employer Account " } });
 
             var submission = new CommitmentSubmission
             {
@@ -91,88 +84,47 @@ namespace SFA.DAS.EmployerCommitments.Application.Commands.SubmitCommitment
                     commitment.ProviderId.GetValueOrDefault(),
                     commitment.ProviderLastUpdateInfo?.EmailAddress ?? string.Empty);
 
-            _logger.Info($"{emails.Count} provider found email address/es");
-
-            //if (!_configuration.CommitmentNotification.SendEmail) return;
+            _logger.Info($"Found {emails.Count} provider email address/es");
 
             var tokens = new Dictionary<string, string> {
-                { "type", message.LastAction == LastAction.Approve ? "approval" : "review" },
-                { "cohort_reference", commitment.Reference },
-                { "first_name",  message.UserDisplayName }
-                //{ "receiving_employer", receivingLegalEntityName}
+                { "cohort_reference", commitment.Reference }
             };
 
             string templateId;
-            if (commitment.AgreementStatus == AgreementStatus.NotAgreed)
+            switch (commitment.AgreementStatus)
             {
-                if (commitment.TransferSender != null)
-                {
-                    templateId = "ProviderTransferCommitmentNotification";
-
+                case AgreementStatus.NotAgreed when commitment.TransferSender != null:
+                    templateId = "TransferProviderCommitmentNotification";
                     tokens["receiving_employer"] = commitment.LegalEntityName;
-                    //var data = await _mediator.SendAsync(new GetTransferRequestQueryRequest
-                    //{
-                    //    AccountId = commitment.EmployerAccountId, // accountId,
-                    //    TransferRequestId = commitment.TransferSender.Id.Value, //transferRequestId,
-                    //    CallerType = CallerType.TransferReceiver
-                    //});
-
-                    //tokens["receiving_employer"] = data.TransferRequest.LegalEntityName;
-                }
-                else
+                    break;
+                case AgreementStatus.NotAgreed:
                     templateId = "ProviderCommitmentNotification";
+                    tokens["type"] = message.LastAction == LastAction.Approve ? "approval" : "review";
+                    break;
+                default:
+                    templateId = "ProviderCohortApproved";
+                    break;
             }
-            else
-                templateId = "ProviderCohortApproved";
 
             foreach (var email in emails)
             {
                 _logger.Info($"Sending email to {email}");
-                //var notificationCommand = BuildNotificationCommand(email, templateId, tokens);
-                    //email,
-                    //commitment,
-                    //message.LastAction, message.UserDisplayName);
                 await _mediator.SendAsync(BuildNotificationCommand(email, templateId, tokens));
             }
         }
 
-        //        private SendNotificationCommand BuildNotificationCommand(string email, CommitmentView commitment, LastAction action, string userDisplayName)
         private SendNotificationCommand BuildNotificationCommand(string email, string templateId, Dictionary<string, string> tokens)
         {
-            //where belongs? only if transfer
-            //var transferRequest = await _commitmentApi.GetTransferRequestForReceiver(commitment.TransferSender.Id.Value, commitment.EmployerAccountId);
-            //var x = transferRequest.LegalEntityName;
-
-            //var data = await _mediator.SendAsync(new GetTransferRequestQueryRequest
-            //{
-            //    AccountId = accountId,
-            //    TransferRequestId = transferRequestId,
-            //    CallerType = callerType
-            //});
-
-
-            //string templateId;
-            //if (commitment.AgreementStatus == AgreementStatus.NotAgreed)
-            //    templateId = commitment.TransferSender != null ? "ProviderTransferCommitmentNotification_dev" : "ProviderCommitmentNotification";
-            //else
-            //    templateId = "ProviderCohortApproved";
-
             return new SendNotificationCommand
             {
                 Email = new Email
                 {
                     RecipientsAddress = email,
-                    TemplateId = templateId, //commitment.AgreementStatus == AgreementStatus.NotAgreed ? "ProviderCommitmentNotification" : "ProviderCohortApproved",
+                    TemplateId = templateId,
                     ReplyToAddress = "noreply@sfa.gov.uk",
                     Subject = "x",
                     SystemId = "x",
                     Tokens = tokens
-                    //Tokens = new Dictionary<string, string> {
-                    //    { "type", action == LastAction.Approve ? "approval" : "review" },
-                    //    { "cohort_reference", commitment.Reference },
-                    //    { "first_name",  userDisplayName },
-                    //    { "receiving_employer", receivingLegalEntityName}
-                    //}
                 }
             };
         }
