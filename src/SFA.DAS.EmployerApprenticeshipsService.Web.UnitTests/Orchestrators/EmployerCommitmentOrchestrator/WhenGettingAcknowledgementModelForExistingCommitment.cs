cdb06a9c3daef65e6fa07d8babd9f3048d379188
phildavies <github@phildavies.co.uk>
2018-05-29 13:57:58 +0100

    add unit test + refactor existing

--- src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Orchestrators/EmployerCommitmentOrchestrator/WhenGettingAcknowledgementModelForExistingCommitment.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Orchestrators/EmployerCommitmentOrchestrator/WhenGettingAcknowledgementModelForExistingCommitment.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using NUnit.Framework;
-using Moq;
+using SFA.DAS.Commitments.Api.Types;
 using SFA.DAS.Commitments.Api.Types.Commitment;
-using SFA.DAS.EmployerCommitments.Application.Queries.GetCommitment;
 
 namespace SFA.DAS.EmployerCommitments.Web.UnitTests.Orchestrators.EmployerCommitmentOrchestrator
 {
@@ -13,18 +11,7 @@ namespace SFA.DAS.EmployerCommitments.Web.UnitTests.Orchestrators.EmployerCommit
         [Test]
         public async Task ThenIsTransferShouldBeSetWhenTransferSenderIdHasValue()
         {
-            MockMediator.Setup(x => x.SendAsync(It.IsAny<GetCommitmentQueryRequest>()))
-                .ReturnsAsync(new GetCommitmentQueryResponse
-                {
-                    Commitment = new CommitmentView
-                    {
-                        TransferSender = new TransferSender
-                        {
-                            Id = 1
-                        },
-                        Messages = new List<MessageView>()
-                    }
-                });
+            CommitmentView.TransferSender = new TransferSender {Id = 1};
 
             //Act
             var result = await EmployerCommitmentOrchestrator.GetAcknowledgementModelForExistingCommitment("ABC123", "XYZ123", "ABC321");
@@ -36,21 +23,22 @@ namespace SFA.DAS.EmployerCommitments.Web.UnitTests.Orchestrators.EmployerCommit
         [Test]
         public async Task ThenIsTransferShouldntBeSetWhenTransferSenderIdHasNoValue()
         {
-            MockMediator.Setup(x => x.SendAsync(It.IsAny<GetCommitmentQueryRequest>()))
-                .ReturnsAsync(new GetCommitmentQueryResponse
-                {
-                    Commitment = new CommitmentView
-                    {
-                        TransferSender = null,
-                        Messages = new List<MessageView>()
-                    }
-                });
-
             //Act
             var result = await EmployerCommitmentOrchestrator.GetAcknowledgementModelForExistingCommitment("ABC123", "XYZ123", "ABC321");
 
             //Assert
             Assert.IsFalse(result.Data.IsTransfer);
+        }
+
+        [Test]
+        [TestCase(AgreementStatus.BothAgreed, true)]
+        public async Task ThenIsSecondApprovalShouldBeSetCorrectly(AgreementStatus agreementStatus, bool expectedIsSecondApproval)
+        {
+            CommitmentView.AgreementStatus = agreementStatus;
+
+            var result = await EmployerCommitmentOrchestrator.GetAcknowledgementModelForExistingCommitment("ABC123", "XYZ123", "ABC321");
+
+            Assert.AreEqual(expectedIsSecondApproval, result.Data.IsSecondApproval);
         }
     }
 }
