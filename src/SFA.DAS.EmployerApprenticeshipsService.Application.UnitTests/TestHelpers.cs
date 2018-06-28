6a32b9a03451362e72d89baeccafa1e8a133bd10
Phil Davies <github@phildavies.co.uk>
2018-06-28 15:39:41 +0100

    move PopulateTemplate into TestHelpers

--- src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/TestHelpers.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/TestHelpers.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace SFA.DAS.EmployerCommitments.Application.UnitTests
 {
@@ -8,6 +9,16 @@ namespace SFA.DAS.EmployerCommitments.Application.UnitTests
         {
             var serialized = JsonConvert.SerializeObject(source);
             return JsonConvert.DeserializeObject<T>(serialized);
+        }
+
+        public static string PopulateTemplate(string template, Dictionary<string, string> tokens)
+        {
+            foreach (var token in tokens)
+            {
+                template = template.Replace($"(({token.Key}))", token.Value);
+            }
+
+            return template;
         }
     }
 }
