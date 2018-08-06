743b555a140d36d5a602176c71c70cb3201edbc8
chrisfoster76 <chrisfoster186@googlemail.com>
2018-08-06 08:56:33 +0100

    Minor tweak

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Extensions/ITrainingProgrammeExtensions.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Extensions/ITrainingProgrammeExtensions.cs
@@ -7,13 +7,8 @@ namespace SFA.DAS.EmployerCommitments.Application.Extensions
     {
         public static bool IsActiveOn(this ITrainingProgramme course, DateTime effectiveDate)
         {
-            if (!course.EffectiveFrom.HasValue || course.EffectiveFrom.Value <= effectiveDate)
-            {
-                if (!course.EffectiveTo.HasValue || course.EffectiveTo.Value >= effectiveDate)
-                {
-                    return true;
-                }
-            }
+            if ((!course.EffectiveFrom.HasValue || course.EffectiveFrom.Value <= effectiveDate) &&
+                (!course.EffectiveTo.HasValue || course.EffectiveTo.Value >= effectiveDate)) return true;
 
             return false;
         }
