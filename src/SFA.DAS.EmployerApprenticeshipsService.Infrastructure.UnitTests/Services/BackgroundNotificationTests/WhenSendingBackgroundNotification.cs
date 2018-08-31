ce01d47f6dd1f4f17fbed1259e37993ba9de5183
Scott Wakefield <scott@expertcc.co.uk>
2018-08-31 15:35:40 +0100

    DPP-1609 added required nugets.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure.UnitTests/Services/BackgroundNotificationTests/WhenSendingBackgroundNotification.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerCommitments.Application.Commands.SendNotification;
+using SFA.DAS.Notifications.Api.Client;
+
+namespace SFA.DAS.EmployerCommitments.Infrastructure.UnitTests.Services.BackgroundNotificationTests
+{
+    [TestFixture]
+    public class WhenSendingBackgroundNotification
+    {
+        [Test, EmployerCommitmentsAutoData, Ignore("todo")]
+        public async Task ThenSendsEmailToNotificationApi(
+            SendNotificationCommand command,
+            [Frozen] Mock<INotificationsApi> mockNotificationsApi,
+            SendNotificationCommandHandler sut)
+        {
+            await sut.Handle(command);
+
+            mockNotificationsApi.Verify(api => api.SendEmail(command.Email), Times.Once);
+        }
+    }
+}
