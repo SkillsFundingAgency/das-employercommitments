25a5a157962eff753b517bdea0c58a2a5b340ff1
Scott Wakefield <scott@expertcc.co.uk>
2018-08-31 10:57:29 +0100

    DPP-1609 started tests for existing SendNotificationCommandHandler.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Commands/SendNotificationTests/WhenHandlingTheCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerCommitments.Application.Commands.SendNotification;
+using SFA.DAS.EmployerCommitments.Application.Validation;
+
+namespace SFA.DAS.EmployerCommitments.Application.UnitTests.Commands.SendNotificationTests
+{
+    [TestFixture]
+    public class WhenHandlingTheCommand
+    {
+        [Test, EmployerCommitmentsAutoData]
+        public async Task ThenItValidatesTheCommand(
+            SendNotificationCommand command,
+            [Frozen] Mock<IValidator<SendNotificationCommand>> mockValidator,
+            SendNotificationCommandHandler sut)
+        {
+            await sut.Handle(command);
+
+            mockValidator.Verify(validator => validator.Validate(command), Times.Once);
+        }
+        // ThenThrowsInvalidRequestExceptionIfInvalid
+        // then sends email to notification
+
+        // then catches exception if api throws
+    }
+}
