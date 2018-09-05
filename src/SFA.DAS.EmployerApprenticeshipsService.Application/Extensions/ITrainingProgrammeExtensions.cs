6668ed60261a55671c9b6224849f6887b658d5d4
Phil Davies <github@phildavies.co.uk>
2018-09-05 08:42:09 +0100

    single source for course/band date calculations

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Extensions/ITrainingProgrammeExtensions.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Extensions/ITrainingProgrammeExtensions.cs
@@ -6,44 +6,41 @@ namespace SFA.DAS.EmployerCommitments.Application.Extensions
 {
     public static class ITrainingProgrammeExtensions
     {
-        public static bool IsActiveOn(this ITrainingProgramme course, DateTime effectiveDate)
+        public static bool IsActiveOn(this ITrainingProgramme course, DateTime date)
         {
-            return DateWithinRange(course.EffectiveFrom, course.EffectiveTo, effectiveDate);
+            return GetStatusOn(course.EffectiveFrom, course.EffectiveTo, date) == TrainingProgrammeStatus.Active;
         }
 
-        public static TrainingProgrammeStatus GetStatusOn(this ITrainingProgramme course, DateTime effectiveDate)
+        public static TrainingProgrammeStatus GetStatusOn(this ITrainingProgramme course, DateTime date)
         {
-            var dateOnly = effectiveDate.Date;
-
-            if (course.EffectiveFrom.HasValue && course.EffectiveFrom.Value.FirstOfMonth() > dateOnly)
-                return TrainingProgrammeStatus.Pending;
-
-            if(!course.EffectiveTo.HasValue || course.EffectiveTo.Value >= dateOnly)
-                return TrainingProgrammeStatus.Active;
-
-            return TrainingProgrammeStatus.Expired;
-
+            return GetStatusOn(course.EffectiveFrom, course.EffectiveTo, date);
         }
 
-        public static int FundingCapOn(this ITrainingProgramme course, DateTime effectiveDate)
+        public static int FundingCapOn(this ITrainingProgramme course, DateTime date)
         {
-            if (!course.IsActiveOn(effectiveDate))
+            //todo: would probably be better to return int? null or throw if out of range
+            if (!course.IsActiveOn(date))
                 return 0;
 
-            var applicableFundingPeriod = course.FundingPeriods.FirstOrDefault(x => DateWithinRange(x.EffectiveFrom, x.EffectiveTo, effectiveDate));
+            var applicableFundingPeriod = course.FundingPeriods.FirstOrDefault(x => GetStatusOn(x.EffectiveFrom, x.EffectiveTo, date) == TrainingProgrammeStatus.Active);
 
             return applicableFundingPeriod?.FundingCap ?? 0;
         }
 
         /// <remarks>
-        /// we make use of the same logic to determinge ActiveOn and FundingBandOn so that if the programme is active, it should fall within a funding band
+        /// we make use of the same logic to determine ActiveOn and FundingBandOn so that if the programme is active, it should fall within a funding band
         /// </remarks>
-        private static bool DateWithinRange(DateTime? effectiveFrom, DateTime? effectiveTo, DateTime date)
+        private static TrainingProgrammeStatus GetStatusOn(DateTime? effectiveFrom, DateTime? effectiveTo, DateTime date)
         {
-            //todo: still have 2 sources of truth (this and GetStatusOn) need only 1, e.g. return GetStatusOn == Active here in effect
             var dateOnly = date.Date;
-            return (!effectiveFrom.HasValue || effectiveFrom.Value.FirstOfMonth() <= dateOnly) &&
-                   (!effectiveTo.HasValue || effectiveTo.Value >= dateOnly);
+
+            if (effectiveFrom.HasValue && effectiveFrom.Value.FirstOfMonth() > dateOnly)
+                return TrainingProgrammeStatus.Pending;
+
+            if (!effectiveTo.HasValue || effectiveTo.Value >= dateOnly)
+                return TrainingProgrammeStatus.Active;
+
+            return TrainingProgrammeStatus.Expired;
         }
     }
 }
