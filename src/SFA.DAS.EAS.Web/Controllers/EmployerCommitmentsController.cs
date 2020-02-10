0755e19c15ead9bf2b1dd008df454d45c3c5937e
chrisfoster186 <chrisfoster186@googlemail.com>
2020-02-10 10:44:44 +0000

    Adds [Deprecated] attribute to log invocations of deprecated actions and the referrrer

--- src/SFA.DAS.EAS.Web/Controllers/EmployerCommitmentsController.cs
+++ src/SFA.DAS.EAS.Web/Controllers/EmployerCommitmentsController.cs
@@ -47,6 +47,7 @@ namespace SFA.DAS.EmployerCommitments.Web.Controllers
 
         [HttpGet]
         [Route("home", Name = "CommitmentsHome")]
+        [Deprecated]
         public ActionResult Index(string hashedAccountId)
         {
             return Redirect(_linkGenerator.CommitmentsV2Link($"{hashedAccountId}"));
@@ -55,6 +56,7 @@ namespace SFA.DAS.EmployerCommitments.Web.Controllers
         [HttpGet]
         [OutputCache(CacheProfile = "NoCache")]
         [Route("cohorts")]
+        [Deprecated]
         public async Task<ActionResult> YourCohorts(string hashedAccountId)
         {
             if (_featureToggleService.Get<EnhancedApprovals>().FeatureEnabled)
@@ -73,6 +75,7 @@ namespace SFA.DAS.EmployerCommitments.Web.Controllers
         [HttpGet]
         [OutputCache(CacheProfile = "NoCache")]
         [Route("cohorts/draft")]
+        [Deprecated]
         public async Task<ActionResult> Draft(string hashedAccountId)
         {
             if (!await IsUserRoleAuthorized(hashedAccountId, Role.Owner, Role.Transactor))
@@ -89,6 +92,7 @@ namespace SFA.DAS.EmployerCommitments.Web.Controllers
 
         [HttpGet]
         [Route("cohorts/review")]
+        [Deprecated]
         public async Task<ActionResult> ReadyForReview(string hashedAccountId)
         {
             if (!await IsUserRoleAuthorized(hashedAccountId, Role.Owner, Role.Transactor ))
@@ -105,6 +109,7 @@ namespace SFA.DAS.EmployerCommitments.Web.Controllers
 
         [HttpGet]
         [Route("cohorts/provider")]
+        [Deprecated]
         public async Task<ActionResult> WithProvider(string hashedAccountId)
         {
             if (!await IsUserRoleAuthorized(hashedAccountId, Role.Owner, Role.Transactor))
@@ -121,6 +126,7 @@ namespace SFA.DAS.EmployerCommitments.Web.Controllers
 
         [HttpGet]
         [Route("cohorts/transferFunded")]
+        [Deprecated]
         public async Task<ActionResult> TransferFunded(string hashedAccountId)
         {
             if (!await IsUserRoleAuthorized(hashedAccountId, Role.Owner, Role.Transactor))
@@ -140,6 +146,7 @@ namespace SFA.DAS.EmployerCommitments.Web.Controllers
 
         [HttpGet]
         [Route("cohorts/rejectedTransfers")]
+        [Deprecated]
         public async Task<ActionResult> RejectedTransfers(string hashedAccountId)
         {
             if (!await IsUserRoleAuthorized(hashedAccountId, Role.Owner, Role.Transactor))
@@ -424,6 +431,7 @@ namespace SFA.DAS.EmployerCommitments.Web.Controllers
         [HttpGet]
         [OutputCache(CacheProfile = "NoCache")]
         [Route("{hashedCommitmentId}/details")]
+        [Deprecated]
         public async Task<ActionResult> Details(string hashedAccountId, string hashedCommitmentId)
         {
             if (!await IsUserRoleAuthorized(hashedAccountId, Role.Owner, Role.Transactor))
---
+++ src/SFA.DAS.EAS.Web/Plumbing/Mvc/DeprecatedAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using NLog;
+
+namespace SFA.DAS.EmployerCommitments.Web.Plumbing.Mvc
+{
+    public class DeprecatedAttribute : ActionFilterAttribute
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            var actionName = filterContext.ActionDescriptor.ActionName;
+
+            var urlReferrer = filterContext.RequestContext.HttpContext.Request.UrlReferrer;
+            var referrer = urlReferrer == null ? "unknown" : urlReferrer.ToString();
+
+            Logger.Warn($"Deprecated action invoked: {controllerName}.{actionName} from {referrer}");
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
